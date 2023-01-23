5b05644862cfadac1157268892fdcd39657fe592
Tom <tomking151183@gmail.com>
2023-01-23 14:10:00 +0000

    adding telephone number to contact form html and php, tested form, working fine

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'markjonesplasteringltd@hotmail.co.uk';
+  $receiving_email_address = 'markjonesplasteringltd.co.uk';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -20,6 +20,7 @@
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
+  $contact->from_telephone = $_POST['tel'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
@@ -34,7 +35,9 @@
   */
 
   $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['tel'], 'Telephone');
   $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['subject'], 'Subject');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
